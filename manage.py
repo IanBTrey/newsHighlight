e885d3dd84b87bddf90434225456ff9c6f83074f
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-19 23:01:00 +0300

    I created a unit test function

--- manage.py
+++ manage.py
@@ -1,2 +1,13 @@
+
+
+def test():
+    '''
+    Run unit tests(this feature is the coolest bana)
+    '''
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+# manager.add_command('server',Server)
 if __name__ == '__main__':
     manager.run()
