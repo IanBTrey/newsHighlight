cc732d8a7d0e76f3d164794676a6869b52e52420
Ian Brian Trey <kibetkirui010@gmail.comgit config --global user.name IanBTrey>
2019-05-19 23:24:54 +0300

    I created a funtion     Function that gets the json response to our url request

--- app/request.py
+++ app/request.py
@@ -78,3 +78,22 @@ def process_results(sources_list):
             sources_results.append(source_object)
 
     return sources_results
+
+def get_articles(query):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_articles_url = article_base_url.format(query,api_key)
+    # print(get_sources_url)
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+
+        articles_results = None
+
+        if get_articles_response['articles']:
+            articles_results_list = get_articles_response['articles']
+            articles_results = process_article_results(articles_results_list)
+
+
+    return articles_results
